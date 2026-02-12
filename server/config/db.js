5d0533a5a06a233bdf40f8d97a094f46d75a0483
guptaaryan8092 <guptaaryan8092@gmail.com>
2026-02-12 17:16:55 +0530

    feat: add MongoDB connection setup with Mongoose and error handling.

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`❌ Database Connection Error: ${error.message}`);
+        console.error(`Database Connection Error: ${error.message}`);
         if (error.message.includes('querySrv ECONNREFUSED')) {
             console.error('TIP: Check your DNS settings or use a non-SRV connection string');
         }
